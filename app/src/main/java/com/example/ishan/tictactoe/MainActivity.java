a92bdd8dd48f3872e3163402322c11f728c5ed24
Ishan-Singh <ishan199518@gmail.com>
2016-08-22 23:30:04 +0530

    game state saving

--- app/src/main/java/com/example/ishan/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/ishan/tictactoe/MainActivity.java
@@ -6,13 +6,35 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    int activeplayer=0;
+    int[] gamestate={2,2,2,2,2,2,2,2,2}; //2 for unplayed state
+    int[][] winpos={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+
     public void dropIn(View view){
+
         ImageView counter=(ImageView)view;
+        int tappedcounter=Integer.parseInt(counter.getTag().toString());
+        if (gamestate[tappedcounter]==2) {
+            gamestate[tappedcounter]=activeplayer;
+
+        }
         counter.setTranslationY(-1000f);
-        counter.setImageResource(R.drawable.yellow_chip);
+        if(activeplayer==0) {
+
+            counter.setImageResource(R.drawable.yellow_chip);
+            activeplayer = 1;
+        }
+        else{
+            counter.setImageResource(R.drawable.redchip);
+            activeplayer = 0;
+
+
+        }
         counter.animate().translationYBy(1000f).setDuration(300);
 
     }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
