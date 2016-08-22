be1658a7f822606e715e179d981bb70ba6a31ed1
Ishan-Singh <ishan199518@gmail.com>
2016-08-23 00:08:19 +0530

    when someone wins

--- app/src/main/java/com/example/ishan/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/ishan/tictactoe/MainActivity.java
@@ -4,11 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
     int activeplayer=0;
     int[] gamestate={2,2,2,2,2,2,2,2,2}; //2 for unplayed state
-    int[][] winpos={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+    int[][] winpos ={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
 
     public void dropIn(View view){
 
@@ -31,8 +32,10 @@ public class MainActivity extends AppCompatActivity {
 
         }
         counter.animate().translationYBy(1000f).setDuration(300);
-        for (int[] winpos: winpos){
+        for ( winpos : winpos){
             if (gamestate[winpos[0]]==gamestate[winpos[1]] && gamestate[winpos[1]]==gamestate[winpos[2]]&&gamestate[winpos[0]]!=2) {
+                //someone won!
+                LinearLayout layout=(LinearLayout)findViewById(R.id.playAgainlayout)
 
             }
         }
